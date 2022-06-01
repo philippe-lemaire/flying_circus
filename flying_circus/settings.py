eef67f22c7abb9b04aface3b87685623b69ddb98
Philippe Lemaire <philippe.lemaire@protonmail.com>
2022-06-01 10:36:50 +0200

    added base template folder

--- flying_circus/settings.py
+++ flying_circus/settings.py
@@ -71,7 +71,9 @@ ROOT_URLCONF = "flying_circus.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "templates",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
