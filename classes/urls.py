ea93018fc8ec4be79f9f99c1cb5d1ba047b49bbc
Philippe Lemaire <philippe.lemaire@protonmail.com>
2022-06-01 10:35:13 +0200

    first real view

--- classes/urls.py
+++ classes/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "classes"
+
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index_classes"),
 ]
