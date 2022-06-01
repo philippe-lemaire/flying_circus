a38da4364fab6357656218f4726a369260ce7573
Philippe Lemaire <philippe.lemaire@protonmail.com>
2022-06-01 12:06:50 +0200

    view to lessons, with dual slug :)

--- classes/urls.py
+++ classes/urls.py
@@ -6,4 +6,9 @@ app_name = "classes"
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index_classes"),
+    path(
+        "<slug:course_slug>/<slug:slug>/",
+        views.LessonDetailView.as_view(),
+        name="lesson_detail",
+    ),
 ]
